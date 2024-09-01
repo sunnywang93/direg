c9a869853410eddd12ee7b50616843c53ab0dfa6
sunnywang93 <sunnywanggw@gmail.com>
2024-09-01 16:27:49 +0200

    Add final aniso detect & clean files

--- R/ani_detect.R
+++ R/ani_detect.R
@@ -9,42 +9,63 @@
 #' @param alpha_adj Numeric, the estimated and correction angle.
 #' @param H_min Numeric, the estimated minimum regularity.
 #' @param H_max Numeric, the estimated maximum regularity.
-#' @param delta Numeric, the spacings used for estimation.
+#' @param delta_grid Vector, the grid of spacings used for estimation.
 #' @param sigma Numeric, the estimated noise.
-#' @returns Numeric, where `0` indicates isotropy, and `1` indicates anisotropy.
+#' @param H_pow Numeric, the power of the rate term.
+#' @returns List, containing the following elements:
+#'  - **$indic**: Numeric, with 1 indicator anisotropy and 0 otherwise.
+#'  - **$tau** : Numeric, the threshold value used to test for anisotropy.
 #' @export
 
 ani_detect <- function(X_list, beta_n, g_adj, alpha_adj,
-                       H_min, H_max, delta, sigma,
-                       H_pow = 1.1) {
+                       H_min, H_max, delta_grid, sigma,
+                       H_pow = 1/3) {
 
-  beta_vec <- runif(n = beta_n, min = alpha_adj + pi/4, max = alpha_adj + 3*pi/4)
+  beta_vec <- runif(n = beta_n,
+                    min = alpha_adj + pi/4,
+                    max = alpha_adj + 3*pi/4)
 
-  M0 <- length(X_list[[1]]$t)
+  M0 <- length(X_list[[1]]$t)^2
 
-  tsub <- seq(0, 1, length.out = floor(M0 / 3))
+  tsub <- seq(0, 1, length.out = floor(sqrt(M0) / 3))
 
   tout <- expand.grid(t1 = tsub,
                       t2 = tsub)
 
-  Hmin_beta <- purrr::map_dbl(beta_vec,
-                              ~H_sheets_dir(X_list = X_list,
-                                            tout = tout,
-                                            delta = delta,
-                                            base_list = list(c(cos(.x), sin(.x))),
-                                            sigma = sigma))
-  # Split into odd and even pairs to avoid dependence in averaging
-  Hmin_odd <- Hmin_beta[2 * seq_len(floor(beta_n/2)) - 1]
-  Hmin_even <- Hmin_beta[2 * seq_len(floor(beta_n/2))]
-  epsilon_beta <- mean(abs(Hmin_odd - Hmin_even), na.rm = TRUE)
+  base_beta <- purrr::map2(cos(beta_vec), sin(beta_vec),
+                           ~c(.x, .y))
+  base_beta_ortho <- purrr::map2(cos(beta_vec + pi/2), sin(beta_vec + pi/2),
+                                 ~c(.x, .y))
 
-  if(abs(H_max - H_min) > epsilon_beta + log(M0)^(-H_pow)) {
-    message("Data is anisotropic")
-    1
+  Hmin_beta <- purrr::map(delta_grid,
+                          ~H_sheets_dir(X_list = X_list,
+                                        tout = tout,
+                                        delta = .x,
+                                        base_list = base_beta,
+                                        sigma = sigma)) |>
+    (\(x) Reduce('+', x) / length(x))()
+
+  Hmin_beta_ortho <- purrr::map(delta_grid,
+                                ~H_sheets_dir(X_list = X_list,
+                                              tout = tout,
+                                              delta = .x,
+                                              base_list = base_beta_ortho,
+                                              sigma = sigma)) |>
+    (\(x) Reduce('+', x) / length(x))()
+
+  epsilon_beta <- mean(abs(Hmin_beta - Hmin_beta_ortho), na.rm = TRUE)
+  tau <- epsilon_beta + exp(-log(M0)^H_pow)
+
+  if(abs(H_max - H_min) > tau) {
+    #message("Data is anisotropic")
+    indic <- 1
   } else {
-    message("Data is isotropic")
-    0
+    #message("Data is isotropic")
+    indic <- 0
   }
+
+  list('indic' = indic,
+       'tau' = tau)
 
 
 }
